ac832b8c5ed71b37f3b9166b420c9db06f30ac2b
jieun <jieun4322@naver.com>
2020-07-26 21:52:36 +0900

    Updates

---
+++ precache-manifest.3b8e7d66a92110682a740aee92a230b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0743b86ca9a6994488592b32ef9ead1c",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "d3bee58458887d52149a",
+    "url": "/movie_app/static/css/main.c257243f.chunk.css"
+  },
+  {
+    "revision": "109356d05735fcb9e967",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3bee58458887d52149a",
+    "url": "/movie_app/static/js/main.51b2ffcb.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
